04f107afe24d53a23a10b97b4f20e1729a268448
周明 <zhouming@zhoumingdeMacBook-Pro.local>
2022-09-25 13:04:35 +0800

    实现connection

--- main.go
+++ main.go
@@ -1,1 +1,9 @@
-package myserv
+package main
+
+import "github.com/CharmingZhou/myserv/snet"
+
+func main() {
+	s := snet.NewServer("[myserv V0.2]")
+	// 2.开启服务
+	s.Serve()
+}
--- siface/connection.go
+++ siface/connection.go
@@ -1,1 +1,15 @@
 package siface
+
+import "net"
+
+// 定义连接接口
+type Connection interface {
+	Start()                         //启动连接，让当前连接开始工作
+	Stop()                          //停止连接，结束当前连接状态M
+	GetTCPConnection() *net.TCPConn //从当前连接获取原始的socket TCPConn
+	GetConnID() uint32              //获取当前连接ID
+	RemoteAddr() net.Addr           //获取远程客户端地址信息
+}
+
+// 定义一个统一处理连接业务的接口
+type HandFunc func(*net.TCPConn, []byte, int) error
